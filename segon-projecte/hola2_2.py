2aae62d4e90a7006f4fdd576863d9bd949fbf308
Aprietos <apsupc@gmail.com>
2026-08-10 21:02:59 +0200

    Add print statement for GitHub modification per fer pull

--- segon-projecte/hola2_2.py
+++ segon-projecte/hola2_2.py
@@ -2,3 +2,4 @@ print ("A veure, aquest es un intent de tornar a crear un document per seguir el
 
 print ("canvis 2.. hola? provant??")
 print ("Vaig a fer la part 3")
+print ("Ara modifico l'arxiu des de Github per fe run pull")
