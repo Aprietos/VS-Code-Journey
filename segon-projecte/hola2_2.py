7103eaad61abbccc4b408ebd2b6175145f98f74f
aprietos <apsupc@gmail.com>
2026-08-10 21:00:57 +0200

    Actualitzo el programa

--- segon-projecte/hola2_2.py
+++ segon-projecte/hola2_2.py
@@ -1,3 +1,4 @@
 print ("A veure, aquest es un intent de tornar a crear un document per seguir el tutorial de chatpgt per fer les funsions de push i pull. A veure que tal...")
 
 print ("canvis 2.. hola? provant??")
+print ("Vaig a fer la part 3")
